9ae6527262364f791da7c3959dd6fc3fecfdbd05
Bladgroen <67104310+Bladgroen@users.noreply.github.com>
2023-08-02 16:30:27 +0200

    🚧started on checkauth hook

---
+++ src/firebase/useAuth.ts
@@ -0,0 +1,19 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const useAuth = (redirectIfNotLoggedIn = true) => {
+  const router = useRouter();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+      } else {
+        if (redirectIfNotLoggedIn) {
+          router.replace("/");
+        }
+      }
+    });
+  });
+};
